3cba45ecdfcadcdae80f65cb911250145abec9b0
jesidurango <jesid.durango@gmail.com>
2012-05-25 21:41:18 -0500

    model changes

--- economy/models.py
+++ economy/models.py
@@ -2,7 +2,10 @@ from sqlalchemy import (
     Column,
     Integer,
     Text,
+    ForeignKey,
+    DECIMAL,
     )
+from sqlalchemy.types import BIGINT
 
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -16,6 +19,12 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
+class BaseEntity(object):
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8'
+    }
+
 class MyModel(Base):
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
@@ -25,4 +34,16 @@ class MyModel(Base):
     def __init__(self, name, value):
         self.name = name
         self.value = value
+
+class Product(Base, BaseEntity):
+    __tablename__ = 'ec_product'
+    id = Column('product_id', BIGINT(unsigned = True), primary_key = True)
+    name = Column(Text, nullable=False)
+    product_value = Column(DECIMAL(precision = 8, scale = 2))
+
+class Calculate(Base, BaseEntity):
+    __tablename__ = 'ec_calculate'
+    id = Column('calculate_id', BIGINT(unsigned = True), primary_key = True)
+    product_id = Column(BIGINT(unsigned = True)), ForeignKey('ec_product.product_id', name='fk_calculate_product',
+            onupdate='RESTRICT', ondelete='RESTRICT', nullable=False)
 
