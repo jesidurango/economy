1c11c9c6a10e026544d33171044ef4be37dd8e3f
jesidurango <jesid.durango@gmail.com>
2012-05-29 00:10:22 -0500

    calculate total value to pay and some jquery with ajax functionality

--- economy/controller/calculate.py
+++ economy/controller/calculate.py
@@ -11,12 +11,17 @@ def calculateInit(request):
         return Response("Error", content_type='text/plain', status_int=500)
     return {'empty':'empty'}
 
-@view_config(route_name='calculate_values', renderer='calculate/calculate.pt')
+@view_config(route_name='calculate_values', renderer='json')
 def calculateValues(request):
     try:
-        produc_value = request.GET.get("txtValorProducto")
+        produc_value = request.POST.get("txtValorProducto")
+        quota_value = request.POST.get("txtCuota")
+        number_quota = request.POST.get("txtNumeroCuotas")
+        total_pay = float(number_quota) * float(quota_value)
+        print request.POST.get("txtCuota")
+        print request.POST.get("txtNumeroCuotas")
         print produc_value
-        new_value = 0
+        new_value = 1234
     except DBAPIError:
         return Response("Error", content_type="text/plain", status_int=500)
-    return {'result': new_value}
+    return {'result': new_value, 'total_pay' : total_pay}
--- economy/static/js/general.js
+++ economy/static/js/general.js
@@ -1,11 +1,38 @@
+function validations () {
+    $("#txtValorProducto").keypress( function(event) {
+        return validateNumericOnly(event);
+    });
+    $("#txtCuota").keypress( function(event) {
+        return validateNumericOnly(event);
+    });
+    $("#txtNumeroCuotas").keypress( function(event) {
+        return validateNumericOnly(event);
+    });
+}
+
+function validateNumericOnly ( event ) {
+    var key = event.which || event.keyCode;
+    //alert(key)
+    if ( ( key <= 57 && key >= 48 ) || ( key <= 40 && key >= 37 ) || key == 8 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function calculate () {
     $.ajax({
-        type: "GET",
+        type: "POST",
         url: urlCalculateValues,
-        data: { txtValorProducto: $("txtValorProducto").value }
-    }).done(function( msg ) {
-        alert( "Response " + msg );
+        data: { txtValorProducto: $("#txtValorProducto").val(),
+            txtCuota: $("#txtCuota").val(),
+            txtNumeroCuotas: $("#txtNumeroCuotas").val() }
+    }).done(function( response ) {
+        calbackCalculate(response);
     });
-    //document.frmCalculte.action = urlCalculateValues;
-    //document.frmCalculte.submit();
 }
+
+function calbackCalculate ( response ) {
+    $("#showResult").html("Usted terminara pagando un total de: " + response.total_pay);
+}
+
